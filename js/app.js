810f3f2541d587af3e146c106cbf6ba950196e72
Lucas Ferreira <lucasferreira241@gmail.com>
2019-02-26 22:40:52 -0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "your API key",
+    authDomain: "yourproject.firebaseapp.com",
+    databaseURL: "https://yourproject.firebaseio.com",
+    projectId: "yourproject",
+    storageBucket: "yourproject.appspot.com",
+    messagingSenderId: "your messaging sender"
+};
+firebase.initializeApp(config);
