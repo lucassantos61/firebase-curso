f36eeefef0a3aa6e9d9e1c887edbf65df7636959
Lucas Ferreira <lucasferreira241@gmail.com>
2019-02-27 22:16:03 -0300

    Adicionando funcionalidade de listar e inserir no firebase

---
+++ js/real-time-database.js
@@ -0,0 +1,26 @@
+var nameInput = document.getElementById('nameInput');
+var ageInput = document.getElementById('ageInput');
+var addButton = document.getElementById('addButton');
+var usersList = document.getElementById('usersList');
+
+addButton.addEventListener('click',function () {
+    create(nameInput.value, ageInput.value)
+});
+
+function create(name, age) {
+    var data = {
+        name:name,
+        age:age
+    };
+
+    return firebase.database().ref().child('users').push(data);
+}
+
+firebase.database().ref('users').on('value',function (snapshot) {
+    usersList.innerHTML = '';
+    snapshot.forEach(function (item) {
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(item.val().name +' : '+item.val().age));
+        usersList.appendChild(li);
+    });
+});
